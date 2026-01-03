ee76185b04dbf755772f71b6593d5d0dad86e613
Oberdan Santos <oberdan@agidesk.com.br>
2026-01-02 23:28:35 -0300

    Class 4.14

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -39,7 +39,7 @@ export default function CityDetailsScreen() {
         touristAttractions={city.touristAttractions}
       />
       <Divider paddingHorizontal="padding" />
-      <CityDetailsMap />
+      <CityDetailsMap location={city.location} />
       <Divider paddingHorizontal="padding" />
       <CityDetailsRelatedCities />
     </Screen>
--- src/components/containers/CityDetailsMap.tsx
+++ src/components/containers/CityDetailsMap.tsx
@@ -1,10 +1,35 @@
+import { useAppTheme } from "@/src/theme/useAppTheme";
+import { City } from "@/src/types";
+import MapView from "react-native-maps";
 import { Box } from "../Box";
 import { Text } from "../Text";
 
-export function CityDetailsMap() {
+type CityDetailsMapProps = Pick<City, "location">;
+
+export function CityDetailsMap({ location }: CityDetailsMapProps) {
+  const { borderRadii } = useAppTheme();
   return (
     <Box padding="padding">
-      <Text>CityDetailsMap</Text>
+      <Text variant="title22" mb="s16">
+        Mapa
+      </Text>
+      <Box
+        style={{
+          overflow: "hidden",
+          borderRadius: borderRadii.default,
+          height: 200,
+        }}
+      >
+        <MapView
+          style={{ width: "100%", height: "100%" }}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </Box>
     </Box>
   );
 }
