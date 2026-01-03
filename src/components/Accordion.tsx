836322e550279b0c00039509f2b980786062dd9d
Oberdan Santos <oberdan@agidesk.com.br>
2026-01-02 22:49:47 -0300

    Class 4.11

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,6 +1,8 @@
 import { Pressable, StyleSheet, View } from "react-native";
 import Animated, {
+  interpolate,
   SharedValue,
+  useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withTiming,
@@ -17,28 +19,44 @@ type AccordionProps = {
 
 export function Accordion({ title, description }: AccordionProps) {
   const isOpen = useSharedValue(false);
+  const progress = useSharedValue(0);
 
   function handleOpenPress() {
     isOpen.value = !isOpen.value;
+    progress.value = withTiming(isOpen.value ? 0 : 1, { duration: 500 });
   }
 
   return (
     <Pressable onPress={handleOpenPress}>
       <View>
-        <AccordionHeader title={title} />
+        <AccordionHeader title={title} progress={progress} />
         <AccordionBody description={description} isOpen={isOpen} />
       </View>
     </Pressable>
   );
 }
 
-function AccordionHeader({ title }: { title: string }) {
+function AccordionHeader({
+  title,
+  progress,
+}: {
+  title: string;
+  progress: SharedValue<number>;
+}) {
+  const iconAnimationStyle = useAnimatedStyle(() => ({
+    transform: [
+      { rotate: `${interpolate(progress.value, [0, 1], [0, -180])}deg` },
+    ],
+  }));
+
   return (
     <View style={styles.header}>
       <Box flexShrink={1}>
         <Text variant="title16">{title}</Text>
       </Box>
-      <Icon name="Chevron-down" color="gray2" />
+      <Animated.View style={iconAnimationStyle}>
+        <Icon name="Chevron-down" color="gray2" />
+      </Animated.View>
     </View>
   );
 }
